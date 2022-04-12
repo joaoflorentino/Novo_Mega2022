4db208078a250480f12909ae0d4217489c28e842
JoaoFlorentino <joao.florentino.senai@gmail.com>
2022-04-12 19:02:38 -0300

    trabalho inicial dos arquivos csv

--- megaLimpezaDados.py
+++ megaLimpezaDados.py
@@ -31,10 +31,41 @@ class PuxaCVS:
         
 
     def buscarCvs(self):
+        '''Funcao de busca de arquivo csv contendo sorteios
+        da megasena '''
         self.dataFrame = pd.read_csv(self.caminho, index_col='Conc.')
         return self.dataFrame.head(5)
-      
 
-df0 = PuxaCVS()
-cabec = df0.buscarCvs()
-print(cabec)
+    def faxinaData(self):
+        '''Funçao cria novo dataframe com elininação dos dados
+        NdN contidos no frame inicial '''
+        self.df1 = self.dataFrame.sort_values(['Conc.'], ascending=True)
+        self.df1.fillna(value=0,  inplace=True)
+        return self.df1.head(8)
+
+    def criaListaSorteio(self):
+        '''Funcao que elimina algumas colunas deixando as dezenas sorteadas
+       e o numero do concurso '''
+        self.sort = []
+        self.df2 = self.df1.drop(['Data', 'Ganhador', 'premio'], axis=1) 
+        return self.df2
+
+    def criaArquivosDeEstudos(self):
+        self.df1.to_csv('data/organizadoDf1.csv')
+        self.df2.to_csv('data/concursosDezenas.csv')
+
+        return
+
+if __name__ == '__main__':
+
+    df0 = PuxaCVS()
+    cabec = df0.buscarCvs()
+    print(cabec)
+    inverte = df0.faxinaData()
+    print(inverte)
+    dezenas = df0.criaListaSorteio()
+    print(dezenas)
+    print(f'Salvando arquivos para outros estudos subsequentes:')
+    df0.criaArquivosDeEstudos()
+
+
